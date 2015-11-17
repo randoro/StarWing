ce160bf53e26c6eef072b041c61a63af69c6e096
Randoro <anton.arnoldsson@hotmail.com>
2015-11-17 16:14:32 +0100

    added ring class

---
+++ Source/StarWing/Ring.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StarWing.h"
+#include "Ring.h"
+
+
+// Sets default values
+ARing::ARing()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARing::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARing::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
